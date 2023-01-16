6d8378a848a2eef91f4cc6886f031b095542dea5
woonrie <120543764+woonrie@users.noreply.github.com>
2023-01-16 18:02:22 -0500

    changes for caption generation
    prompt processing is not functional, needs complete rework to take in entire message object.

--- afd_script.py
+++ afd_script.py
@@ -1,17 +1,13 @@
 import discord
 import openai
+import os
 from io import BytesIO
 import requests
 from discord.ext import commands
 
-tkn = "TOKEN"
-openai.api_key = "KEY"
+tkn = os.environ.get('bot_token')
+openai.api_key = os.environ.get('openai_key')
 client = discord.Client(intents=discord.Intents.all())
-commands = {
-    "help": "shows all commands",
-    "gu" : "generates unique image with GPT based prompt",
-    "gp [prompt]" : "generates image based on user prompt" 
-}
 
 def process_command(content):
     '''
@@ -22,25 +18,30 @@ def process_command(content):
 
     Returns:
         ret (list): ret[0] == 0: no prompt, message ret[1]
-                    ret[0] ==1: has prompt, prompt ret[1]
+                    ret[0] == 1: has prompt, prompt ret[1]
+                    ret[0] == 2: has image, image ret[1]
         
     '''
     command = content.lstrip("!afd ")
     ret = [0]
 
     if command in ("help", "?"):
-        ret.append("Apollo DALL-E Commands (!afd [command]):\n\t\"gu\" : Generates a unique image using an AI generated prompt\
-            \n\t\"gp [prompt]\" : Creates an image based on an entered prompt.")
+        ret.append("Apollo DALL-E Commands (!afd [command]):\n\t\"gen-u\" : Generates a unique image using an AI generated prompt\
+            \n\t\"gen-p [prompt]\" : Creates an image based on an entered prompt.\
+                \n\tcaption [image]: Generates a caption for an image.")
 
-    elif command == "gu": # generate random art image - returns a unique prompt
+    elif command == "gen-u": # generate random art image - returns a unique prompt
         ret[0] = 1
         ret.append(openai.Completion.create(model="text-davinci-003", prompt="Generate a short, extremely unique and creative image caption \
             that doubles as a DALLE prompt", temperature=1.0, max_tokens=100).choices[0].text)
 
-    elif command.startswith("gp"): # generate img based on prompt - returns user prompt
+    elif command.startswith("gen-p"): # generate img based on prompt - returns user prompt
         ret[0] = 1
-        ret.append(command.lstrip("gp "))
+        ret.append(command.lstrip("gen-p "))
         
+    elif command.startswith("caption"):
+        
+      
     else: # command not recognized
         ret.append("Sorry, I couldn't recognize your command. Please check your message or enter \"!afd help\" for a list of all commands.")
     
@@ -57,6 +58,7 @@ def get_image(prompt):
     Returns:
         image (discord.File): the image file
     '''
+    
     response = openai.Image.create(
         prompt=prompt,
         size="1024x1024", 
@@ -67,12 +69,26 @@ def get_image(prompt):
     image_data = requests.get(image_url).content
     image = discord.File(BytesIO(image_data), filename='image.jpg')
     return image
+j
 
+def get_caption(image_url):
+    '''
+    Generates a caption for an image
+    
+    '''
+    response = openai.Completion.create(
+        prompt=("Generate a caption for this image" + image_url),
+        temperature=0.5,
+        max_tokens = 100
+    )
+    caption = response.choices[0].text
+    return caption
+    
     
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online)
-    print("AFD has connected to Discord")
+    print("AFD LIVE")
 
 
 @client.event
